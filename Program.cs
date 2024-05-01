79b0c0412385ed290e2bf8f289c661291f875ff8
l-pacm <lpacciulli.m@gmail.com>
2024-05-01 11:54:38 -0300

    Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Iphone - FEITO
+Iphone cel1 = new Iphone("99877474","Iphone7","22222222",128);
+Console.WriteLine(cel1.Numero);
+cel1.Ligar();
+cel1.InstalarAplicativo("Telegram");
+
+Nokia cel2 = new Nokia("988475632","N500","55555555",256);
+Console.WriteLine(cel2.Numero);
+cel2.ReceberLigacao();
+cel2.InstalarAplicativo("Instagram");
